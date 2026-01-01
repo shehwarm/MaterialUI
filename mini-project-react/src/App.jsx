96ebddd3041d30d2323cf9dbb84837c7867ae8e8
Shehwar Maryam <shehwarm93@gmail.com>
2026-01-01 17:48:54 +0500

    first commit

---
+++ mini-project-react/src/App.jsx
@@ -0,0 +1,13 @@
+import Button from '@mui/material/Button';
+
+
+function App(){
+  return (
+    <>
+       <h1>Hello World</h1>
+       <Button>Click me</Button>
+    </>
+  );
+}
+
+export default App;
