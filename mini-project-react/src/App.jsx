c41083b91c7f6b7d1df4affd5edb6fba8b237d92
Shehwar Maryam <shehwarm93@gmail.com>
2026-01-01 17:59:03 +0500

    Refactor button component to include icon and handle click event

--- mini-project-react/src/App.jsx
+++ mini-project-react/src/App.jsx
@@ -1,13 +1,24 @@
 import Button from '@mui/material/Button';
-
+import DeleteIcon from '@mui/icons-material/Delete';
 
 function App(){
+  let handleClick = () => {
+    alert("Button clicked!");
+  }
+
   return (
     <>
        <h1>Hello World</h1>
-       <Button>Click me</Button>
+       <Button 
+       variant="contained" 
+       onClick={handleClick} 
+       startIcon={<DeleteIcon />} 
+       color="error" 
+       size="small">
+       delete
+       </Button>
     </>
   );
 }
 
-export default App;
+export default App; 
