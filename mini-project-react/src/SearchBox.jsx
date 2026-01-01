7d8b3da5f8466150a4361ed9d239422ae750346c
Shehwar Maryam <shehwarm93@gmail.com>
2026-01-01 18:30:59 +0500

    Add SearchBox component with weather search functionality

--- mini-project-react/src/SearchBox.jsx
+++ mini-project-react/src/SearchBox.jsx
@@ -1,13 +1,27 @@
+import  { useState } from "react";
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import SendIcon from '@mui/icons-material/Send';
+import "./SearchBox.css"
 
 export default function SearchBox(){
+    let [city, setCity] = useState("");
+
+    let handleChange = (event) =>{
+        setCity(event.target.value);
+    };
+
+    let handleSubmit = (event) =>{ 
+        event.preventDefault();
+        console.log(city);
+        setCity("");
+    };
+
     return (
         <div className="SearchBox">
-            <h3>Search for the weather</h3>
-            <form>
-                <TextField id="city" label="City Name" variant="outlined" required/>
+            <h1>Search for the weather</h1>
+            <form onSubmit={handleSubmit}>
+                <TextField id="city" label="City Name" variant="outlined" required value={city} onChange={handleChange}/>
                 <br></br><br></br>
                 <Button variant="contained" endIcon={<SendIcon />} size="small"> Send</Button>
             </form>
